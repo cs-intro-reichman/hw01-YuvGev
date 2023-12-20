ef60f2fecae53906da070595e659b9178bce9db6
YuvGev <yuval.geva01@post.runi.ac.il>
2023-12-20 14:44:29 +0200

    first upload to github

--- GenThree.java
+++ GenThree.java
@@ -5,5 +5,25 @@
 public class GenThree {
 	public static void main(String[] args) {
 		// Put your code here	
+	int a = Integer.parseInt(args[0]); 
+	int b = Integer.parseInt(args[1]);
+
+	int maxVal = Math.max(a, b);
+	int minVal = Math.min(a, b);
+
+	int num1 = (int) ( Math.random()*(maxVal - minVal + 1) + minVal );
+	int num2 = (int) ( Math.random()*(maxVal - minVal + 1) + minVal );
+	int num3 = (int) ( Math.random()*(maxVal - minVal + 1) + minVal );
+		
+	System.out.println(num1);
+	System.out.println(num2);
+	System.out.println(num3);
+		
+	int minNum = Math.min(Math.min(num1, num2), Math.min(num2, num3));
+		
+	System.out.println("The minimal generated number was " + minNum);
+		
+
 	}
 }
+
--- LinearEq.java
+++ LinearEq.java
@@ -6,4 +6,15 @@
  */
 public class LinearEq {
 	// Put your code here
+	public static void main (String[] args){
+
+	double a = Double.parseDouble(args[0]); 
+	double b = Double.parseDouble(args[1]);
+	double y = Double.parseDouble(args[2]);
+	double x = (y - b)/a;
+
+	System.out.println(a + " * x + " + b + " = " + y);
+	System.out.println("x = " + x);
+
+	}
 }
